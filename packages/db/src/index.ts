0325f9390c553b44bc5f32e0007e397cc4c1934e
wolowizard010 <jasdeep97jan@gmail.com>
2026-05-26 13:25:35 +0530

    phase 1 complete

---
+++ packages/db/src/index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = db;
+}
+
+export * from '@prisma/client';
